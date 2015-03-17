41e4caaa2cb9592fe662b7272e29226df2480dc1
Sherrie Jones <sj24kt@comcast.net>
2015-03-17 12:37:28 -0600

    initial commit - setup basic storyboard

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by Sherrie Jones on 3/17/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
